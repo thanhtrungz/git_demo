0862f068aae8cef2960adda3f9bcf407a56220bd
Thanh Trung <thanhtrungpham2001@gmail.com>
2026-04-19 15:57:31 +0700

    Create conflict code in test.py

--- test.py
+++ test.py
@@ -4,4 +4,8 @@ def print_sum(a:int , b:int) -> int:
     print(f"a + b = {a+b}")
     
 def track():
+<<<<<<< HEAD
     print("Tracking !!!")
+=======
+    print("Nothing to show here")
+>>>>>>> 22b78f0 (Create conflict code in test.py)
